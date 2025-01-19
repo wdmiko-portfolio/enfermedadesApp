bae40c1ad5e184366b67627c7074fb1c06d1fd78
wdmiko-portfolio <wdmiko@gmail.com>
2025-01-19 00:35:05 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'AppEnfermedades',
+  webDir: 'www'
+};
+
+export default config;
